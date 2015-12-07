d6d78b896c545372de4eede556f19e68ff412e21
jawittdesigns <Killjoy11>
2015-12-07 07:55:23 -0500

    Added footer subtext

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 <?php
-  $ch_smokehouse_text = dfw_get_field( 'ch_smokehouse_text', true );
-  $ch_smokehouse_link = dfw_get_field( 'ch_smokehouse_link', true );
+  $ch_smokehouse_text    = dfw_get_field( 'ch_smokehouse_text', true );
+  $ch_smokehouse_subtext = dfw_get_field( 'ch_smokehouse_subtext', true );
+  $ch_smokehouse_link    = dfw_get_field( 'ch_smokehouse_link', true );
 ?>
 </main>
 <footer class="footer" role="contentinfo">
@@ -13,7 +14,13 @@
   <?php endif; ?>
   <div class="content__wrapper">
     <?php get_template_part( 'template-parts/menu', 'footer' ); ?>
-    <p class="tidesans__600"><a href="<?php echo $ch_smokehouse_link; ?>" class="link__hover--shadow" rel="nofollow"><?php echo $ch_smokehouse_text; ?></a></p>
+    <p class="tidesans__600 link__hover--shadow footer__text">
+      <a href="<?php echo $ch_smokehouse_link; ?>" rel="nofollow"><?php echo $ch_smokehouse_text; ?></a>
+    </p>
+    <p class="tidesans__400 link__hover--shadow footer__subtext">
+      <a href="<?php echo $ch_smokehouse_link; ?>" rel="nofollow">
+        <?php echo $ch_smokehouse_subtext; ?></p>
+      </a>
     <p class="copyright tidesans__400">&copy; <?php _e( 'Commonhouse Ales', 'fcwp'); ?> <?php echo date('Y'); ?></p>
   </div>
 </footer>
