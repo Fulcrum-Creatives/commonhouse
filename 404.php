98969e439f06a318675c870568d94a753bb60dde
jawittdesigns <Killjoy11>
2015-12-08 11:08:45 -0500

    Added 404 page

--- 404.php
+++ 404.php
@@ -1,8 +1,18 @@
 <?php get_header(); ?>
-<main id="main" class="main" role="main">
-  <div class="content__wrapper">
-    <h1><?php _e('Page Not Found', 'fcwp'); ?></h1>  
-    <p><?php _e('Sorry, but the page you requested has not been found', 'fcwp'); ?></p>
-  </div>
-</main>
+<div class="content__wrapper inner">
+  <article id="post-<?php the_ID(); ?>" <?php post_class('entry single'); ?> aria-labelledby="section-heading-<?php the_ID(); ?>" role="article">
+      <div class="entry__image col__1-2">
+        
+      </div>
+      <div class="entry__content col__1-2">
+      <p>
+        <?php echo _e( 'Rather tipsy, Lenny stumbled around the corner—into a plate glass door. When he came to, he saw his uncommon reflection and strange surroundings.', 'fcwp' ); ?>
+      </p>
+      <p>
+        <?php echo _e( 'Want to go <a href="' . home_url() . '"><strong>home</strong></a>?', 'fcwp' ); ?>
+      </p>
+      </div>
+    </section>
+  </article>
+</div>
 <?php get_footer(); ?>
--- header.php
+++ header.php
@@ -25,38 +25,61 @@ if( !is_home() || !is_front_page() ) {
     }
   }
 </style>
-<header class="header bg_lines" role="banner" style="background-color:<?php echo $ch_header_color; ?>">
-  <div class="content__wrapper">
-    <div class="header__logo">
-      <div class="banner__left"></div>
-      <?php get_template_part( 'template-parts/logo' ); ?>
-      <div class="banner__right"></div>
-    </div>
-    <?php get_template_part( 'template-parts/menu', 'primary' ); ?>
-    <div class="header__banner page-<?php echo $page_id; ?>">
-      <div class="header__banner-text">
-        <?php if( $ch_tag_line != '' ) : ?>
-          <p class="banner-text__tagline tidesans__500 <?php echo $header_show; ?>">
-            <?php echo $ch_tag_line; ?>
-          </p>
-        <?php endif;  if( $ch_header_text_line_one != '' ) : ?>
-          <p class="banner-text__line-one tidesans__700 <?php echo $header_show; ?>">
-            <?php echo $ch_header_text_line_one; ?>
-          </p>
-          <p class="banner-text__line-two tidesans__700 <?php echo $header_show; ?>">
-            <?php echo $ch_header_text_line_two; ?>
-          </p>
-        <?php endif;  if( $ch_sub_tag_line != '' ) : ?>
-          <p class="banner-text__sub-tagline tidesans__500 <?php echo $header_show; ?>">
-            <?php echo $ch_sub_tag_line; ?>
-          </p>
+<?php if( !is_404() ) : ?>
+  <header class="header bg_lines" role="banner" style="background-color:<?php echo $ch_header_color; ?>">
+    <div class="content__wrapper">
+      <div class="header__logo">
+        <div class="banner__left"></div>
+        <?php get_template_part( 'template-parts/logo' ); ?>
+        <div class="banner__right"></div>
+      </div>
+      <?php get_template_part( 'template-parts/menu', 'primary' ); ?>
+      <div class="header__banner page-<?php echo $page_id; ?>">
+        <div class="header__banner-text">
+          <?php if( $ch_tag_line != '' ) : ?>
+            <p class="banner-text__tagline tidesans__500 <?php echo $header_show; ?>">
+              <?php echo $ch_tag_line; ?>
+            </p>
+          <?php endif;  if( $ch_header_text_line_one != '' ) : ?>
+            <p class="banner-text__line-one tidesans__700 <?php echo $header_show; ?>">
+              <?php echo $ch_header_text_line_one; ?>
+            </p>
+            <p class="banner-text__line-two tidesans__700 <?php echo $header_show; ?>">
+              <?php echo $ch_header_text_line_two; ?>
+            </p>
+          <?php endif;  if( $ch_sub_tag_line != '' ) : ?>
+            <p class="banner-text__sub-tagline tidesans__500 <?php echo $header_show; ?>">
+              <?php echo $ch_sub_tag_line; ?>
+            </p>
+          <?php endif; ?>
+        </div>
+        <?php if( $ch_header_image != '' ) : ?>
+          <div class="header__banner-image"></div>
         <?php endif; ?>
       </div>
-      <?php if( $ch_header_image != '' ) : ?>
-        <div class="header__banner-image"></div>
-      <?php endif; ?>
     </div>
-  </div>
-</header>
+  </header>
+<?php else : ?>
+  <header class="header bg_lines" role="banner">
+    <div class="content__wrapper">
+      <div class="header__logo">
+        <div class="banner__left"></div>
+        <?php get_template_part( 'template-parts/logo' ); ?>
+        <div class="banner__right"></div>
+      </div>
+      <?php get_template_part( 'template-parts/menu', 'primary' ); ?>
+      <div class="header__banner page-<?php echo $page_id; ?>">
+        <div class="header__banner-text">
+          <p class="banner-text__line-one tidesans__700 <?php echo $header_show; ?>">
+            <?php echo _e( 'WRONG TURN', 'fcwp' ); ?>
+          </p>
+          <p class="banner-text__line-two tidesans__700 <?php echo $header_show; ?>">
+            <?php echo _e( 'A.K.A. FOR AU FAUX', 'fcwp' ); ?>
+          </p>
+        </div>
+      </div>
+    </div>
+  </header>
+<?php endif; ?>
 
 <main id="main" class="main" role="main">
