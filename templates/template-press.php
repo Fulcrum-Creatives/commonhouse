b50899cb7d272efe8f9da5837baf020a2eca1167
jawittdesigns <Killjoy11>
2016-01-20 16:21:43 -0500

    Fixed Links

--- templates/template-press.php
+++ templates/template-press.php
@@ -7,12 +7,30 @@ get_header();
 <div class="content__wrapper inner">
   <div class="col__1-3 news__aside">
     <div class="cust-border__top"></div>
+      <h2 class="news__aside-heading lulocleanone__bold">
+        <?php echo _e( 'Press Kit', 'fcwp' ); ?>
+      </h2>
       <div class="button_one lulocleanone__bold">
+        <div class="button_one lulocleanone__bold">
+        <a href="<?php echo $ch_pp_logos['url']; ?>" class="link__hover--shadow-small">
+          <?php echo _e( 'Logos', 'fcwp' ); ?>
+        </a>
+      </div>
+      <div class="button_one lulocleanone__bold">
+        <a href="<?php echo $ch_pp_images['url']; ?>" class="link__hover--shadow-small">
+          <?php echo _e( 'Photos', 'fcwp' ); ?>
+        </a>
+    <div class="cust-border__bottom"></div>
+    </div>
         <a href="<?php echo home_url(); ?>/commonhouse-news" class="link__hover--shadow-small">
           <?php echo _e( 'News Releases', 'fcwp' ); ?>
         </a>
       </div>
-    <div class="cust-border__bottom"></div>
+    <div class="button_one lulocleanone__bold">
+      <a href="<?php echo $ch_lennys_blog_url; ?>" class="link__hover--shadow-small">
+        <?php echo _e( 'Read Lenny\'s Blog', 'fcwp' ); ?>
+      </a>
+    </div>
   </div>
   <div class="col__2-3">
     <?php
